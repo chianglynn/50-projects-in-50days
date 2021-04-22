623c0475406750be0ca8f4ec665202be5d1e916f
Chiang Lynn <lynnchiangtw@gmail.com>
2021-04-22 16:02:41 +0800

    Refactor if statement to ternary operator and add comments

--- 18-background-slider/script.js
+++ 18-background-slider/script.js
@@ -5,26 +5,6 @@ const rightBtn = document.getElementById('right');
 
 let activeSlide = 0;
 
-rightBtn.addEventListener('click', () => {
-    activeSlide++;
-    if (activeSlide > slides.length - 1) {
-        activeSlide = 0;
-    }
-    setBgToBody();
-    setActiveSlide();
-})
-
-leftBtn.addEventListener('click', () => {
-    activeSlide--;
-    if (activeSlide < 0) {
-        activeSlide = slides.length - 1;
-    }
-    setBgToBody();
-    setActiveSlide();
-})
-
-setBgToBody();
-
 function setBgToBody() {
     body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 }
@@ -33,3 +13,19 @@ function setActiveSlide() {
     slides.forEach(slide => slide.classList.remove('active'));
     slides[activeSlide].classList.add('active');
 }
+
+// On load
+setBgToBody();
+
+// Event Listener
+rightBtn.addEventListener('click', () => {
+    activeSlide < slides.length - 1 ? activeSlide++ : activeSlide = 0;
+    setBgToBody();
+    setActiveSlide();
+});
+
+leftBtn.addEventListener('click', () => {
+    activeSlide > 0 ? activeSlide-- : activeSlide = slides.length - 1;
+    setBgToBody();
+    setActiveSlide();
+});
