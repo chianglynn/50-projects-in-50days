6f1fea8ff0e662d1b470af46ce6190937c805559
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-16 16:10:02 +0800

    Rearrange code

--- 09-sound-board/script.js
+++ 09-sound-board/script.js
@@ -1,23 +1,27 @@
+const btnContainer = document.getElementById('buttons');
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
-
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-
-    btn.innerText = sound;
-
-    btn.addEventListener('click', () => {
-        stopAudios();
-        document.getElementById(sound).play();
-    })
-
-    document.getElementById('buttons').appendChild(btn);
-})
 
 function stopAudios() {
     sounds.forEach(sound => {
         const audio = document.getElementById(sound);
         audio.pause();
         audio.currentTime = 0;
-    })
+    });
 }
+
+function generateBtnsForSounds() {
+    sounds.forEach(sound => {
+        const btn = document.createElement('button');
+        btn.innerText = sound;
+        btn.classList.add('btn');
+        btnContainer.appendChild(btn);
+
+        btn.addEventListener('click', () => {
+            stopAudios();
+            const audio = document.getElementById(sound);
+            audio.play();
+        });
+    });
+}
+
+generateBtnsForSounds();
