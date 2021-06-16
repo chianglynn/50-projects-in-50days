81cb544ddba858182b4919a7b622ef4170a4c917
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-16 14:56:18 +0800

    Rearrange code

--- 06-scroll-animation/script.js
+++ 06-scroll-animation/script.js
@@ -2,17 +2,13 @@ const boxes = document.querySelectorAll('.box');
 
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
-    boxes.forEach((box) => {
+    boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
 
-        if (boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-    })
+        if (boxTop < triggerBottom) box.classList.add('show')
+        else box.classList.remove('show')
+    });
 }
 
 checkBoxes();
-
 window.addEventListener('scroll', checkBoxes);
