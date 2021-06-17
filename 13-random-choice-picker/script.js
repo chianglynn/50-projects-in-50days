2c8fe04626116f21bfcffc6b21b6c94833f26294
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-17 14:52:24 +0800

    Rearrange code

--- 13-random-choice-picker/script.js
+++ 13-random-choice-picker/script.js
@@ -1,50 +1,16 @@
 const tagsElement = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
-textarea.focus();
-
-textarea.addEventListener('keyup', (event) => {
-    createTags(event.target.value);
-    if (event.key === 'Enter') {
-        setTimeout(() => {
-            event.target.value = '';
-        }, 10);
-        randomSelect();
-    }
-});
-
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
-    console.log(tags);
 
-    tagsElement.innerText = ''
-
+    tagsElement.innerText = '';
     tags.forEach(tag => {
         const tagElement = document.createElement('span');
         tagElement.classList.add('tag');
         tagElement.innerText = tag;
         tagsElement.appendChild(tagElement);
-    })
-}
-
-function randomSelect() {
-    const times = 30;
-
-    const interval = setInterval(() => {
-        const randomTag = pickRandomTag();
-        highlightTag(randomTag);
-        setTimeout(() => {
-            unhighlightTag(randomTag);
-        }, 100);
-    }, 100);
-
-    setTimeout(() => {
-        clearInterval(interval);
-        setTimeout(() => {
-            const randomTag = pickRandomTag();
-            highlightTag(randomTag);
-        }, 100);
-    }, times * 100);
+    });
 }
 
 function pickRandomTag() {
@@ -59,3 +25,29 @@ function highlightTag(tag) {
 function unhighlightTag(tag) {
     tag.classList.remove('highlight');
 }
+
+function randomSelect() {
+    const times = 30;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+        setTimeout(() => unhighlightTag(randomTag), 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+textarea.focus();
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+    if (e.key === 'Enter') {
+        setTimeout(() => e.target.value = '', 10);
+        randomSelect();
+    }
+});
