bc89ffce0fc6eed0efbd873679154434d7b5d0d0
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-22 14:28:00 +0800

    Rearrange code and add solution 2

--- 21-drag-n-drop/script.js
+++ 21-drag-n-drop/script.js
@@ -1,16 +1,6 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
-
-for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-
 function dragStart() {
     this.className += ' hold';
     setTimeout(() => this.className = 'invisible', 0);
@@ -37,3 +27,22 @@ function dragDrop() {
     this.className = 'empty';
     this.append(fill);
 }
+
+// Solution 1
+// for (const empty of empties) {
+//     empty.addEventListener('dragover', dragOver);
+//     empty.addEventListener('dragenter', dragEnter);
+//     empty.addEventListener('dragleave', dragLeave);
+//     empty.addEventListener('drop', dragDrop);
+// }
+
+// Solution 2
+empties.forEach(empty => {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+});
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
