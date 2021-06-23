3500fc740ca55ba65cb2e89ca0eb5283376214a7
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-23 15:27:59 +0800

    Rearrange code and add solution 2

--- 25-sticky-navbar/script.js
+++ 25-sticky-navbar/script.js
@@ -1,11 +1,23 @@
 const nav = document.querySelector('.nav');
-window.addEventListener('scroll', fixNav);
 
-function fixNav() {
-    // if (window.scrollY > nav.offsetHeight + 150) {
-    //     nav.classList.add('active');
-    // } else {
-    //     nav.classList.remove('active');
-    // }
-    return window.scrollY > nav.offsetHeight + 150 ? nav.classList.add('active') : nav.classList.remove('active');
-}
+// Solution 1
+// function fixNav() {
+//     return window.scrollY > nav.offsetHeight + 150 ? nav.classList.add('active') : nav.classList.remove('active');
+// }
+
+// window.addEventListener('scroll', fixNav);
+
+// Solution 2
+const header = document.querySelector('.hero');
+const navHeight = nav.getBoundingClientRect().height;
+const fixNav = entries => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) nav.classList.add('active');
+    else nav.classList.remove('active');
+};
+const headerObserver = new IntersectionObserver(fixNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
