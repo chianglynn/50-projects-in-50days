9b0bd4d31bf2023a7d24c5857878490eb6e5ab43
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-23 13:40:57 +0800

    Rearrange code

--- 24-content-placeholder/script.js
+++ 24-content-placeholder/script.js
@@ -1,15 +1,11 @@
-
 const header = document.getElementById('header');
 const title = document.getElementById('title');
 const excerpt = document.getElementById('excerpt');
 const profile_img = document.getElementById('profile-img');
 const name = document.getElementById('name');
 const date = document.getElementById('date');
-
 const animated_bgs = document.querySelectorAll('.animated-bg');
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
-
-setTimeout(getData, 2000);
 
 function getData() {
     header.innerHTML = `<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
@@ -23,3 +19,5 @@ function getData() {
     animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
     animated_bgs_texts.forEach(bg => bg.classList.remove('animated-bg-texts'));
 }
+
+setTimeout(getData, 2000);
