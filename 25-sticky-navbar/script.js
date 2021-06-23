a0e4089ae980652b39e5f43f63b4d6c6a5cbfb3a
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-23 16:43:51 +0800

    Add comments

--- 25-sticky-navbar/script.js
+++ 25-sticky-navbar/script.js
@@ -1,13 +1,13 @@
 const nav = document.querySelector('.nav');
 
-// Solution 1
+// Solution 1: always fix navbar
 // function fixNav() {
 //     return window.scrollY > nav.offsetHeight + 150 ? nav.classList.add('active') : nav.classList.remove('active');
 // }
 
 // window.addEventListener('scroll', fixNav);
 
-// Solution 2
+// Solution 2: fix navbar when header and navbar do not intersect each other
 const header = document.querySelector('.hero');
 const navHeight = nav.getBoundingClientRect().height;
 const fixNav = entries => {
