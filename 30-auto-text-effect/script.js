f24a17461d57ac75065c960eb84b7ddfa4ca5583
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-26 07:50:52 +0800

    Refactor code

--- 30-auto-text-effect/script.js
+++ 30-auto-text-effect/script.js
@@ -1,18 +1,18 @@
-
 const textEl = document.getElementById('text');
 const speedEl = document.getElementById('speed');
-
 const text = 'We Love Programming!';
 let index = 1;
-let speed = 300 / speedEl.value;
+let speed;
 
-writeText();
+function changeSpeed() {
+    return speed = 300 / speedEl.value;
+}
 
 function writeText() {
     textEl.innerText = text.slice(0, index);
-    index++;
-    if (index > text.length) index = 1;
-    setTimeout(writeText, speed)
+    index >= text.length ? index = 1 : index++;
+    setTimeout(writeText, changeSpeed());
 }
 
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);
+writeText();
+speedEl.addEventListener('input', changeSpeed);
