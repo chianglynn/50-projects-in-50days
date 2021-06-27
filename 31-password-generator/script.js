cf764b4d09d3993741860872ba9e3fccbc045b54
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-27 10:42:16 +0800

    Rearrange code

--- 31-password-generator/script.js
+++ 31-password-generator/script.js
@@ -6,7 +6,6 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
-
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -14,48 +13,7 @@ const randomFunc = {
     symbol: getRandomSymbol
 };
 
-clipboardEl.addEventListener('click', () => {
-    const textarea = document.createElement('textarea');
-    const password = resultEl.innerText;
-
-    if (!password) return;
-
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-    alert('Password copied to clipboard!')
-});
-
-generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
-    const hasNumber = numbersEl.checked;
-    const hasSymbol = symbolsEl.checked;
-
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = '';
-    const typesCount = lower + upper + number + symbol;
-    const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
-
-    if (typesCount === 0) return '';
-
-    for (let i = 0; i < length; i += typesCount) {
-        typesArray.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        });
-    }
-    const finalPassword = generatedPassword.slice(0, length);
-    return finalPassword;
-}
-
-// HTML ASCII Reference: https://www.w3schools.com/charsets/ref_html_ascii.asp
+// HTML ASCII Reference: https:/ / www.w3schools.com / charsets / ref_html_ascii.asp
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -72,3 +30,47 @@ function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+function generateFinalPassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) return '';
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
+function generatePassword() {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generateFinalPassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+}
+
+function copyPassword() {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+    if (!password) return;
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!')
+}
+
+generateEl.addEventListener('click', generatePassword);
+clipboardEl.addEventListener('click', copyPassword);
