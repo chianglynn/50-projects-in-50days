a40cf5f746c0e44743e57e1fcd791315bd7d3a63
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-01 06:35:04 +0800

    Rearrange code and add comment

--- 41-verify-account-ui/script.js
+++ 41-verify-account-ui/script.js
@@ -1,14 +1,12 @@
 const codes = document.querySelectorAll('.code');
 
 codes[0].focus();
-
 codes.forEach((code, index) => {
     code.addEventListener('keydown', (e) => {
         if (e.key >= 0 && e.key <= 9) {
-            codes[index].value = '';
+            codes[index].value = ''; // Modify current number directly
             setTimeout(() => codes[index + 1].focus(), 10);
-        } else if (e.key === 'Backspace') {
-            setTimeout(() => codes[index - 1].focus(), 10)
         }
-    })
+        if (e.key === 'Backspace') setTimeout(() => codes[index - 1].focus(), 10);
+    });
 });
