a1bf2e6b3232ec7f5cae334f1c8c60e0b7924384
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-30 13:54:59 +0800

    Rearrange code

--- 36-hoverboard/script.js
+++ 36-hoverboard/script.js
@@ -2,14 +2,8 @@ const container = document.getElementById('container');
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const SQUARES = 500;
 
-for (let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-
-    square.addEventListener('mouseover', () => setColor(square));
-    square.addEventListener('mouseout', () => removeColor(square));
-
-    container.appendChild(square);
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function setColor(element) {
@@ -23,6 +17,12 @@ function removeColor(element) {
     element.style.boxShadow = '0 0 2px #000';
 }
 
-function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseout', () => removeColor(square));
+
+    container.appendChild(square);
 }
