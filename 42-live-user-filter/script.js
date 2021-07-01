ac6dabf0a3b2ddeca1bed60719004ac9d9a90d85
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-01 10:53:56 +0800

    Rearrange code

--- 42-live-user-filter/script.js
+++ 42-live-user-filter/script.js
@@ -2,19 +2,14 @@ const result = document.getElementById('result');
 const filter = document.getElementById('filter');
 const listItems = [];
 
-getData();
-filter.addEventListener('input', (e) => filterData(e.target.value));
-
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50');
     const { results } = await res.json();
 
-    // clear loding...
-    result.innerHTML = '';
+    result.innerHTML = ''; // clear loding...
 
     results.forEach(user => {
         const li = document.createElement('li');
-
         listItems.push(li);
         li.innerHTML = `
             <img src="${user.picture.large}" alt="${user.name.first}">
@@ -29,10 +24,10 @@ async function getData() {
 
 function filterData(searchTerm) {
     listItems.forEach(item => {
-        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-            item.classList.remove('hide');
-        } else {
-            item.classList.add('hide');
-        }
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) item.classList.remove('hide');
+        else item.classList.add('hide');
     });
 }
+
+getData();
+filter.addEventListener('input', (e) => filterData(e.target.value));
