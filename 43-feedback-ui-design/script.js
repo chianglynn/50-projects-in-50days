e08214980a68204e860e5cf9370797e87e7d461a
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-02 09:00:05 +0800

    Fix errors for selecting ratings and displaying feedback

--- 43-feedback-ui-design/script.js
+++ 43-feedback-ui-design/script.js
@@ -4,22 +4,35 @@ const sendBtn = document.getElementById('send');
 const panel = document.getElementById('panel');
 let selectedRating = 'Satisfied';
 
+function removeActive() {
+    ratings.forEach(rating => rating.classList.remove('active'));
+}
+
 // Solution 1: using event bubbling
-ratingsContainer.addEventListener('click', (e) => {
-    if (e.target.parentNode.classList.contains('rating')) {
-        removeActive();
-        e.target.parentNode.classList.add('active');
-        selectedRating = e.target.nextElementSibling.innerHTML;
-    }
-});
+// ratingsContainer.addEventListener('click', (e) => {
+//     removeActive();
+//     e.target.parentNode.classList.add('active');
+//     // Click rating containers
+//     if (e.target.classList.contains('rating')) selectedRating = e.target.querySelector('.rating-text').innerHTML;
+//     // Click images
+//     if (e.target.parentNode.classList.contains('rating') && e.target.classList.contains('rating-img')) selectedRating = e.target.nextElementSibling.innerHTML;
+//     // Click rating texts
+//     if (e.target.parentNode.classList.contains('rating') && e.target.classList.contains('rating-text')) selectedRating = e.target.innerHTML;
+// });
 
 // Solution 2: using forEach
-// ratings.forEach(rating => {
-//     rating.addEventListener('click', (e) => {
-//         removeActive();
-//         e.target.parentNode.classList.add('active');
-//     });
-// });
+ratings.forEach(rating => {
+    rating.addEventListener('click', (e) => {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        // Click rating containers
+        if (e.target.classList.contains('rating')) selectedRating = e.target.querySelector('.rating-text').innerHTML;
+        // Click images
+        if (e.target.parentNode.classList.contains('rating') && e.target.classList.contains('rating-img')) selectedRating = e.target.nextElementSibling.innerHTML;
+        // Click rating texts
+        if (e.target.parentNode.classList.contains('rating') && e.target.classList.contains('rating-text')) selectedRating = e.target.innerHTML;
+    });
+});
 
 sendBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
@@ -30,7 +43,3 @@ sendBtn.addEventListener('click', (e) => {
         <p>We'll use your feedback to improve our customer support</p>
     `;
 });
-
-function removeActive() {
-    ratings.forEach(rating => rating.classList.remove('active'));
-}
