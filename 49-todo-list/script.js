97a2d0c80728155d89ee29df045fe3efb3517633
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-04 07:51:53 +0800

    Rearrange code

--- 49-todo-list/script.js
+++ 49-todo-list/script.js
@@ -1,61 +1,44 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todosUL = document.getElementById('todos');
-
+const todosUl = document.getElementById('todos');
 const todos = JSON.parse(localStorage.getItem('todos'));
 
-if (todos) {
-    todos.forEach(todo => addTodo(todo));
+function updateLS() {
+    const todos = [];
+    const todosEl = document.querySelectorAll('li');
+    todosEl.forEach(todoEl => todos.push({
+        text: todoEl.textContent,
+        completed: todoEl.classList.contains('completed'),
+    }));
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    addTodo();
-});
-
 function addTodo(todo) {
-    let todoText = input.value;
-
-    if (todo) {
-        todoText = todo.text;
-    }
+    let todoText = todo ? todo.text : input.value;
 
     if (todoText) {
         const todoEl = document.createElement('li');
-
-        if (todo && todo.completed) {
-            todoEl.classList.add('completed');
-        }
-
+        if (todo && todo.completed) todoEl.classList.add('completed');
         todoEl.textContent = todoText;
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
             updateLS();
         });
-
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
             updateLS();
         });
 
-        todosUL.appendChild(todoEl);
+        todosUl.appendChild(todoEl);
         input.value = '';
         updateLS();
     }
 }
 
-function updateLS() {
-    todosEl = document.querySelectorAll('li');
-
-    const todos = [];
-
-    todosEl.forEach(todoEl => todos.push({
-        text: todoEl.textContent,
-        completed: todoEl.classList.contains('completed')
-    }));
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-}
+if (todos) todos.forEach(todo => addTodo(todo));
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+});
