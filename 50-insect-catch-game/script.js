8370b6b0d565db3038ae9da7002b819638fd1690
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-05 14:36:47 +0800

    Rearrange code

--- 50-insect-catch-game/script.js
+++ 50-insect-catch-game/script.js
@@ -5,24 +5,18 @@ const game_container = document.getElementById('game-container');
 const timeEl = document.getElementById('time');
 const scoreEl = document.getElementById('score');
 const message = document.getElementById('message');
-
 let seconds = 0;
 let score = 0;
 let selected_insect = {};
 
-start_btn.addEventListener('click', () => screens[0].classList.add('up'));
 
-choose_insect_btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const img = btn.querySelector('img');
-        const src = img.getAttribute('src');
-        const alt = img.getAttribute('alt');
-        selected_insect = { src, alt };
-        screens[1].classList.add('up');
-        setTimeout(createInsect, 1000);
-        startGame();
-    })
-});
+function getRandomLocation() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const x = Math.random() * (width - 200) + 100;
+    const y = Math.random() * (height - 200) + 100;
+    return { x, y };
+}
 
 function createInsect() {
     const insect = document.createElement('div');
@@ -35,21 +29,6 @@ function createInsect() {
     game_container.appendChild(insect);
 }
 
-function getRandomLocation() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const x = Math.random() * (width - 200) + 100
-    const y = Math.random() * (height - 200) + 100
-    return { x, y }
-}
-
-function catchInsect() {
-    increaseScore();
-    this.classList.add('caught');
-    setTimeout(this.remove(), 2000);
-    addInsects();
-}
-
 function addInsects() {
     setTimeout(createInsect, 1000);
     setTimeout(createInsect, 1500);
@@ -57,14 +36,15 @@ function addInsects() {
 
 function increaseScore() {
     score++;
-    if (score > 19) {
-        message.classList.add('visible');
-    }
+    if (score > 19) message.classList.add('visible');
     scoreEl.textContent = `Score: ${score}`;
 }
 
-function startGame() {
-    setInterval(increaseTime, 1000);
+function catchInsect() {
+    increaseScore();
+    this.classList.add('caught');
+    setTimeout(this.remove(), 2000);
+    addInsects();
 }
 
 function increaseTime() {
@@ -75,3 +55,20 @@ function increaseTime() {
     timeEl.textContent = `Time: ${m}:${s}`;
     seconds++;
 }
+
+function startGame() {
+    setInterval(increaseTime, 1000);
+}
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'));
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        selected_insect = { src, alt };
+        screens[1].classList.add('up');
+        setTimeout(createInsect, 1000);
+        startGame();
+    });
+});
