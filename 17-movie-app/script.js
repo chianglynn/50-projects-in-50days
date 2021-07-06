97ee66429e63dd24d38e86525d2464514e4c3282
Chiang Lynn <lynnchiangtw@gmail.com>
2021-07-06 08:26:18 +0800

    Rearrange code

--- 17-movie-app/script.js
+++ 17-movie-app/script.js
@@ -6,9 +6,9 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
 function getClassByRate(vote) {
-    if (vote >= 8) return 'green'
-    else if (vote >= 5) return 'orange'
-    else return 'red'
+    if (vote >= 8) return 'green';
+    else if (vote >= 5) return 'orange';
+    else return 'red';
 }
 
 function showMovies(movies) {
@@ -35,7 +35,6 @@ function showMovies(movies) {
 async function getMovies(url) {
     const response = await fetch(url);
     const data = await response.json();
-
     showMovies(data.results);
 }
 
@@ -46,10 +45,9 @@ function getSearchMovies(e) {
     if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm);
         search.value = '';
-    } else {
-        window.location.reload();
     }
+    else window.location.reload();
 }
 
 getMovies(API_URL);
-form.addEventListener('submit', (e) => getSearchMovies(e));
+form.addEventListener('submit', getSearchMovies);
